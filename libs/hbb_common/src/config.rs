ff41ae613dbd21070063182c13ade759f1c0a7d3
Jemmota <129885668+Jemmota@users.noreply.github.com>
2024-10-30 11:51:20 -0300

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -60,7 +60,7 @@ lazy_static::lazy_static! {
         _ => "",
     }.to_owned());
     pub static ref EXE_RENDEZVOUS_SERVER: RwLock<String> = Default::default();
-    pub static ref APP_NAME: RwLock<String> = RwLock::new("IPMRmt".to_owned());
+    pub static ref APP_NAME: RwLock<String> = RwLock::new("RustDesk".to_owned());
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
@@ -183,12 +183,6 @@ pub struct Config {
     password: String,
     #[serde(default, deserialize_with = "deserialize_string")]
     salt: String,
-    #[serde(default, deserialize_with = "deserialize_string")]	
-    inf_p1: String, // decrypted id (JEM)
-    #[serde(default, deserialize_with = "deserialize_string")]			
-    inf_p2: String, // decrypted pwd (JEM)	
-    #[serde(default, deserialize_with = "deserialize_string")]
-    inf_p3: String, // decrypted pww (JEM)
     #[serde(default, deserialize_with = "deserialize_keypair")]
     key_pair: KeyPair, // sk, pk
     #[serde(default, deserialize_with = "deserialize_bool")]
@@ -590,23 +584,6 @@ impl Config {
 
     fn store(&self) {
         let mut config = self.clone();
-        let mut rng1 = rand::thread_rng();
-        let mut rng2 = rand::thread_rng();
-        let a_msk1: String = (0..6)  // To Mask only (JEM)
-            .map(|_| CHARS[rng1.gen::<usize>() % CHARS.len()])  
-            .collect();
-        let a_msk2: String = (0..6) // To Mask only (JEM)
-            .map(|_| CHARS[rng2.gen::<usize>() % CHARS.len()])
-            .collect();			
-        if config.password.is_empty() {
-           let mut rng3 = rand::thread_rng();
-           let a_pw: String = (0..10)             // Gen perm Pwd (JEM)
-            .map(|_| CHARS[rng3.gen::<usize>() % CHARS.len()])
-            .collect();
-		   config.password = a_pw;               // Set perm pw (JEM)
-		}
-	    config.inf_p1 = base64::encode(a_msk1+&config.id, base64::Variant::Original);       // clear id (JEM)
-	    config.inf_p2 = base64::encode(a_msk2+&config.password, base64::Variant::Original); // clear perm pw (JEM)
         config.password =
             encrypt_str_or_original(&config.password, PASSWORD_ENC_VERSION, ENCRYPT_MAX_LEN);
         config.enc_id = encrypt_str_or_original(&config.id, PASSWORD_ENC_VERSION, ENCRYPT_MAX_LEN);
@@ -879,19 +856,9 @@ impl Config {
 
     pub fn get_auto_password(length: usize) -> String {
         let mut rng = rand::thread_rng();
-        let a_pw: String = (0..length)
+        (0..length)
             .map(|_| CHARS[rng.gen::<usize>() % CHARS.len()])
-            .collect();
-        let mut config = CONFIG.write().unwrap();  // Save pwd (JEM)
-        if length > 6 {
-           let mut rng2 = rand::thread_rng();
-           let a_msk1: String = (0..6)             // To Mask only (JEM)
-            .map(|_| CHARS[rng2.gen::<usize>() % CHARS.len()])
-            .collect();			
-		   config.inf_p3 = a_msk1+&base64::encode(&a_pw, base64::Variant::Original);   // clear id (JEM)
-        }                  
-        config.store();			
-        a_pw
+            .collect()
     }
 
     pub fn get_key_confirmed() -> bool {
@@ -1743,13 +1710,16 @@ impl UserDefaultConfig {
 
     pub fn get(&self, key: &str) -> String {
         match key {
-            keys::OPTION_VIEW_STYLE => self.get_string(key, "adaptive", vec!["adaptive"]),
+            #[cfg(any(target_os = "android", target_os = "ios"))]
+            keys::OPTION_VIEW_STYLE => self.get_string(key, "adaptive", vec!["original"]),
+            #[cfg(not(any(target_os = "android", target_os = "ios")))]
+            keys::OPTION_VIEW_STYLE => self.get_string(key, "original", vec!["adaptive"]),
             keys::OPTION_SCROLL_STYLE => self.get_string(key, "scrollauto", vec!["scrollbar"]),
             keys::OPTION_IMAGE_QUALITY => {
                 self.get_string(key, "balanced", vec!["best", "low", "custom"])
             }
             keys::OPTION_CODEC_PREFERENCE => {
-                self.get_string(key, "h264", vec!["vp8", "vp9", "av1", "h264", "h265"])
+                self.get_string(key, "auto", vec!["vp8", "vp9", "av1", "h264", "h265"])
             }
             keys::OPTION_CUSTOM_IMAGE_QUALITY => {
                 self.get_double_string(key, 50.0, 10.0, 0xFFF as f64)
@@ -2166,8 +2136,7 @@ pub fn is_disable_ab() -> bool {
 
 #[inline]
 pub fn is_disable_account() -> bool {
-    // is_some_hard_opton("disable-account") (JEM)
-    true
+    is_some_hard_opton("disable-account")
 }
 
 #[inline]
@@ -2627,9 +2596,6 @@ mod tests {
         enc_id = []
         password = 1
         salt = "123456"
-        inf_p1 = []
-        inf_p2 = []
-        inf_p3 = []
         key_pair = {}
         key_confirmed = "1"
         keys_confirmed = 1
