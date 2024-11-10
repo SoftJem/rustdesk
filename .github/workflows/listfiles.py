7adc0762025e0baee305b27975f39b96c6f3e2b3
Jemmota <129885668+Jemmota@users.noreply.github.com>
2024-11-10 13:07:23 -0300

    Update listfiles.py

--- .github/workflows/listfiles.py
+++ .github/workflows/listfiles.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 
 def listar_arquivos():
-    print("Executando listfiles.py")  # Confirma que o script iniciou
-    caminho_pasta = Path('.\\flutter\\windows\\flutter\\').resolve()
+    print("Executando listfiles.py")
+    caminho_pasta = Path('.\\flutter\\').resolve()
     print(f"Procurando arquivos em: {caminho_pasta}")
 
     if caminho_pasta.exists():
+        encontrou_arquivos = False
         for file in caminho_pasta.rglob('*'):
             if file.is_file():
+                encontrou_arquivos = True
                 print(file)
+        if not encontrou_arquivos:
+            print("Nenhum arquivo encontrado.")
     else:
         print("Caminho não encontrado.")
 
