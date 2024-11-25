3bc51211d97f09f230dfc2e87cb9533a36d79af1
Jemmota <129885668+Jemmota@users.noreply.github.com>
2024-11-25 10:23:20 -0300

    Rvt

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -2131,7 +2131,7 @@ pub fn is_incoming_only() -> bool {
         .read()
         .unwrap()
         .get("conn-type")
-        .map_or(true, |x| x == ("incoming"))
+        .map_or(false, |x| x == ("incoming"))
 }
 
 #[inline]
@@ -2159,8 +2159,7 @@ pub fn is_disable_tcp_listen() -> bool {
 
 #[inline]
 pub fn is_disable_settings() -> bool {
-    //is_some_hard_opton("disable-settings")
-    true
+    is_some_hard_opton("disable-settings")
 }
 
 #[inline]
